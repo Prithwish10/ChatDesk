b8323bc27c16997f4623ee958ece72c14a128467
prithwish-das <prithwishdas60@gmail.com>
2023-05-23 01:10:51 +0530

    Added error handler middlewares

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -3,11 +3,11 @@ import morgan from "morgan";
 import cors from "cors";
 import routes from "../routes/index";
 import config from "../config/index";
-// import {
-//   handle404Error,
-//   handle422Error,
-//   handleError,
-// } from "../middleware/error-handler.middleware";
+import {
+  handle404Error,
+  handle422Error,
+  handleError,
+} from "../middlewares/error-handler.middleware";
 
 export default ({ app }: { app: Application }) => {
   app.enable("trust proxy");
@@ -22,11 +22,11 @@ export default ({ app }: { app: Application }) => {
   app.use(config.api.prefix, routes());
 
   // catch 422 Error and forward to error handler
-  //   app.use(handle422Error);
+  app.use(handle422Error);
 
   // catch 404 Error
-  //   app.use(handle404Error);
+  app.use(handle404Error);
 
   // catch other Errors
-  //   app.use(handleError);
+  app.use(handleError);
 };
