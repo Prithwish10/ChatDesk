dd43e133d8a6345a86664742f0a322ec7aabeb78
Prithwish <prithwishdas60@gmail.com>
2023-07-21 01:14:30 +0530

    Updated common library

--- common/src/index.ts
+++ common/src/index.ts
@@ -15,6 +15,7 @@ export { Logger } from "./utils/Logger";
 export * from "./events/Listener";
 export * from "./events/Publisher.ts";
 export * from "./interfaces/User-created-event";
+export * from "./interfaces/User-updated-event";
 export * from "./interfaces/Conversation-created-event"
 export * from "./interfaces/Conversation-updated-event";
 export * from "./enums/subjects";
