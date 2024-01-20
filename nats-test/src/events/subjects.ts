017fdeb343b2d549c6d8a0dc17e18141ed9f0f6b
Prithwish Das <59892611+Prithwish10@users.noreply.github.com>
2024-01-20 20:35:12 +0530

    Delete nats-test directory

--- nats-test/src/events/subjects.ts
+++
@@ -1,6 +0,0 @@
-export enum Subjects {
-  UserCreated = "user:created",
-  UserUpdated = "user:updated",
-  ConversationCreated = "conversation:created",
-  ConversationUpdated = "conversation:updated",
-}
--- nats-test/src/events/user-created-publisher.ts
+++
@@ -1,7 +0,0 @@
-import { Publisher } from './base-publisher';
-import { UserCreatedEvent } from './user-created-event';
-import { Subjects } from './subjects';
-
-export class UserCreatedPublisher extends Publisher<UserCreatedEvent> {
-  subject: Subjects.UserCreated = Subjects.UserCreated;
-}
