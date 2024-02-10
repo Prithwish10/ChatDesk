cdf363e7aecc7610c85a685487f4721712d170ac
Prithwish <prithwishdas60@gmail.com>
2024-02-10 20:45:46 +0530

    Modifies signup page and api integration

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,7 +6,7 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <Component currentUser={currentUser} {...pageProps} />
     </div>
   );
 };
@@ -23,7 +23,11 @@ AppComponent.getInitialProps = async (appContext) => {
 
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(
+      appContext.ctx,
+      client,
+      currentUser
+    );
   }
 
   return {
